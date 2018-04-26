052ed944de6b750d3c5ffc267c348b3e17b4bb7e
Chris Stringham <C.J.Stringham@gmail.com>
2018-04-26 12:33:31 -0700

    setup header changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+  
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="left brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li><a href="#">Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +29,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
